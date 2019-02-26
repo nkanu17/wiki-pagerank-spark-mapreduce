9235436477c40bd1fd30b09d6a55e011a4577ee4
nkanu17 <47899917+nkanu17@users.noreply.github.com>
2019-02-26 17:18:17 +0000

    Update OutputMapper.java
    comments

--- uog-bigdata/src/main/java/mapreduce/OutputMapper.java
+++ uog-bigdata/src/main/java/mapreduce/OutputMapper.java
@@ -1,3 +1,8 @@
+/**
+ *  Group: UG37 Members: Nitin Kanukolanu (2416724K), Shawn Yeng Wei Xen (2395121Y)
+ *  File: OutputMapper.java 
+ *  This file functions as the final mapper that produces the output with just the article key and page rank
+ */
 package mapreduce;
 
 import java.io.IOException;
@@ -10,14 +15,19 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Mapper.Context;
 
 public class OutputMapper extends Mapper<LongWritable, Text, Text, Text> {
-	
+	/* 
+	 * @param <key: line/index number as Text, page rank and list of outlinks as TextArrayWritable>
+	 * @output <key: article title as Text, value: page rank as TextArrayWritable> 
+	 */
 	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 		
+		//reading in the input and removing all outlinks  
 		String line = value.toString();
 		String[] tokens = line.split(",,,|\t");
 		String articleKey = tokens[0];
 		String pageRank = tokens[1];
-				
+		
+		//output is just the article and its page rank
 		context.write(new Text(articleKey), new Text(pageRank));
 	}
 }
