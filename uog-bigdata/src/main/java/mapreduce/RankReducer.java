64043270ff598c3efca1afac56628012121d8799
nkanu17 <47899917+nkanu17@users.noreply.github.com>
2019-02-26 17:16:20 +0000

    Update RankReducer.java
    comments

--- uog-bigdata/src/main/java/mapreduce/RankReducer.java
+++ uog-bigdata/src/main/java/mapreduce/RankReducer.java
@@ -1,13 +1,13 @@
 /**
- * File: RankReducer.java
- * This java file functions as the second reducer, inputting the records from the second mapper. It
- * calculates the page rank based on the number of outlinks of the articles and outputs records
- * similar to the records being output to the second mapper from the first reducer
- * with different page rank values.
+ *  Group: UG37 Members: Nitin Kanukolanu (2416724K), Shawn Yeng Wei Xen (2395121Y)
+ *  File: RankReducer.java 
+ *  This file functions as the reducer that calculates the page rank. 
+ *  Receives the output from the RankMapper and computes the page rank according to the number of iterations. 
+ *  Page Rank formula is shown in the read me file.
+ *  The output is the article title, page rank, and outlinks (if present) for every article present.
  */
-
 package mapreduce;
-
+ 
 import java.io.IOException;
 
 import org.apache.hadoop.io.Text;
@@ -15,6 +15,10 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Reducer.Context;
 
 public class RankReducer extends Reducer<Text, Text, Text, Text> {
+	/*
+	 * @param <key: line/index number as Text, page rank and list of outlinks as TextArrayWritable, context as Context>
+	 * @output  <key: article title as Text, value: page rank and list of outlinks as TextArrayWritable>
+	 */
 	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
 		double oldPageRank= 0.0;
@@ -29,21 +33,22 @@ public class RankReducer extends Reducer<Text, Text, Text, Text> {
 		String articleKey = key.toString();
 
 
-
+		// Checking every node and computing page rank
 		for (Text value : values) {
 
 			String line = value.toString();
 			String[] tokens = line.split(",,,|\t");
 			oldPageRank = Double.parseDouble(tokens[0]);
 
-			// if it is not the outlinks
-
+			// if this is one of the inlinks (not original article title
+			// the page rank is being computed
 			if(!line.contains("#outlinks")) {
 				
 				countOutlinks = Integer.parseInt(tokens[1]);
 				sumScore = sumScore + (oldPageRank/countOutlinks);
 				pageRank = 0.15 + (0.85 *sumScore);
 
+			// if it is the orginal article key with all the outlinks	
 			}else {
 
 				articleKey = key.toString();
@@ -55,11 +60,13 @@ public class RankReducer extends Reducer<Text, Text, Text, Text> {
 						outlinks = outlinks + tokens[i] + ",,,";
 					}				
 				}
+				// if page rank is 0 meaning no references for a certain article
 				if(pageRank==0) {
 					pageRank = 0.15;
 				}							
 			}			
 		}
+		//final key is the article title and value is the page rank and outlinks
 		finalValue = new Text(pageRank + ",,," + outlinks);
 		context.write(key, finalValue);
 	}
